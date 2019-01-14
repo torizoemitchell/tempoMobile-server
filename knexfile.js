69739989718e8246c8f7f2e668e11aa4a03bd123
Tori Mitchell <tori.zoe.mitchell@gmail.com>
2019-01-14 08:13:52 -0700

    add knexfile

---
+++ knexfile.js
@@ -0,0 +1,11 @@
+module.exports = {
+    development: {
+        client: 'pg',
+        connection: 'postgres://localhost/tempomobile'
+    },
+    test: {},
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL
+    }
+}

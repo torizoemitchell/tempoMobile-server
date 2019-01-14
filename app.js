924ec2425a29d9bc63a72c22b9243a55a5f895dd
Tori Mitchell <tori.zoe.mitchell@gmail.com>
2019-01-14 14:53:14 -0700

    routes hooked up

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var usersRouter = require('./routes/users')
+var entriesRouter = require('./routes/entries')
+
 var app = express();
 
 // view engine setup
@@ -20,7 +23,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use('/users', usersRouter)
+app.use('/entries', entriesRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/entries.js
+++ routes/entries.js
@@ -3,22 +3,22 @@ const router = express.Router()
 const knex = require('../knex')
 // READ ALL records for this table
 router.get('/', (req, res, next) => {
-  res.send('ALL RECORDS')
+    res.send('ALL RECORDS')
 })
 // READ ONE record for this table
 router.get('/:id', (req, res, next) => {
-  res.send('ONE RECORD')
+    res.send('ONE RECORD')
 })
 // CREATE ONE record for this table
 router.post('/', (req, res, next) => {
-  res.send('CREATED RECORD')
+    res.send('CREATED RECORD')
 })
 // UPDATE ONE record for this table
 router.put('/:id', (req, res, next) => {
-  res.send('UPDATED RECORD')
+    res.send('UPDATED RECORD')
 })
 // DELETE ONE record for this table
 router.delete('/:id', (req, res, next) => {
-  res.send('DELETED RECORD')
+    res.send('DELETED RECORD')
 })
 module.exports = router

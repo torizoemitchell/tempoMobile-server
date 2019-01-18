c34e5fa90ebbe2a9546be28758be60cdc7d1422b
Tori Mitchell <tori.zoe.mitchell@gmail.com>
2019-01-18 12:48:38 -0700

    adjusted seeds and time zone

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,27 @@ const express = require('express')
 const router = express.Router()
 const knex = require('../knex')
 
-// READ ONE record for this table
+//**********Get user info*************
 router.get('/:id', (req, res, next) => {
-  res.send('ONE RECORD')
+  knex.column('id', 'name', 'cycle_length').select().from('users').where({
+    id: req.params.id
+  }).then((response) => {
+    res.json(response)
+      // let data = []
+      // rows.forEach((obj) => {
+      //   let newObj = {
+      //     id: obj.id,
+      //     name: obj.name,
+      //     message: obj.message
+      //   }
+      //   data.push(newObj)
+      // })
+      // res.json(data)
+    })
+    .catch((err) => {
+      next(err)
+    })
+
 })
 // CREATE ONE record for this table
 router.post('/', (req, res, next) => {
